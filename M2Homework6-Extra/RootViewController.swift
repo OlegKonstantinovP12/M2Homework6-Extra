8cdb7c2a0572dd628b641bff11b837a0fc414b35
Oleg Konstantinov <oleg.boss@gmail.com>
2025-11-15 19:02:16 +0100

    Initial Commit

---
+++ M2Homework6-Extra/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Homework6-Extra
+//
+//  Created by Oleg Konstantinov on 15.11.2025.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
